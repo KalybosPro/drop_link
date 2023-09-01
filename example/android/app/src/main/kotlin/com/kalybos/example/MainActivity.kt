c762f6d021403f1f59ed50e2626c07fdb04bab3f
pahianyo <pahianyo@dactechnologies.net>
2023-09-01 18:24:39 +0000

    first commit

---
+++ example/android/app/src/main/kotlin/com/kalybos/example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kalybos.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/drop_zone.dart
@@ -0,0 +1,85 @@
+// ignore: avoid_web_libraries_in_flutter
+import 'dart:html' as html;
+
+import 'package:flutter/material.dart';
+
+import 'dispatcher.dart';
+
+typedef VoidCallback<T> = void Function(T);
+
+class DropZone extends StatefulWidget {
+  final Widget child;
+  final void Function()? onDragEnter;
+  final void Function()? onDragExit;
+
+  final VoidCallback<String>? onDrop;
+
+  const DropZone({
+    required this.child,
+    Key? key,
+    this.onDrop,
+    this.onDragEnter,
+    this.onDragExit,
+  }) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => DropZoneState();
+}
+
+class DropZoneState extends State<DropZone> {
+  late int id;
+  bool _dragInBounds = false;
+
+  Rect? _getGlobalPaintBounds() {
+    final renderObject = context.findRenderObject();
+    final translation = renderObject?.getTransformTo(null).getTranslation();
+
+    if (translation != null && renderObject?.paintBounds != null) {
+      return renderObject?.paintBounds
+          .shift(Offset(translation.x, translation.y));
+    }
+
+    return null;
+  }
+
+  @override
+  void dispose() {
+    Dispatcher.shared.removeZone(id);
+
+    super.dispose();
+  }
+
+  bool _isCursorWithinBounds(html.MouseEvent value) {
+    final g = _getGlobalPaintBounds();
+
+    return g != null && //
+        value.layer.x > g.left &&
+        value.layer.x < g.right &&
+        value.layer.y > g.top &&
+        value.layer.y < g.bottom;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    id = Dispatcher.shared.addZone(onDragOver: (e) {
+      final tmp = _isCursorWithinBounds(e);
+      if (!_dragInBounds && tmp) {
+        widget.onDragEnter?.call();
+      } else if (_dragInBounds && !tmp) {
+        widget.onDragExit?.call();
+      }
+
+      _dragInBounds = tmp;
+    }, onDrop: (e) async {
+      if (_isCursorWithinBounds(e)) {
+        final data = e.dataTransfer.getData("text");
+        widget.onDrop?.call(data);
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext c) => widget.child;
+}
